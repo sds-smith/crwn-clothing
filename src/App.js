f0550ed0f1cc5d6e322fdbdf290fd5d61c345eff
Shawn Smith <sds.smith24@gmail.com>
2022-06-07 08:41:11 -0400

    add router and outlet, navigation, shop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './categories.styles.scss'
-import Directory from './components/directory/directory'
+import { Routes, Route, Outlet } from 'react-router-dom'
+import Home from './routes/home/home.component'
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
 
 const App =  () => {
   
   return (
-    <Directory />
+    <Routes>
+      <Route path='/' element={ <Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+      
+    </Routes>
+   
   );
 }
 
