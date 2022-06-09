b105b4ebbd406ceff665ac276320384ad1233dad
Shawn Smith <sds.smith24@gmail.com>
2022-06-09 12:47:44 -0400

    add useContext in sign up form and navigation components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -17,9 +17,17 @@ const Navigation = () => {
             <Link className='nav-link' to='/shop'>
                 SHOP
             </Link>
-            <Link className='nav-link' to='/auth'>
-                SIGN IN
-            </Link>
+            { 
+              currentUser ? (
+                <span className='nav-link' >SIGN OUT</span>
+              ) :
+              (
+                <Link className='nav-link' to='/auth'>
+                    SIGN IN
+                </Link>
+              )
+            }
+
           </div>
 
         </div>
