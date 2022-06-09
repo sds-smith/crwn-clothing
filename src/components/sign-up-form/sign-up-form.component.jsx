8c26b694a488a7d37dd6d6e947cacee00b881fb7
Shawn Smith <sds.smith24@gmail.com>
2022-06-09 07:01:35 -0400

    fix circular call in createAuthUserWithEmailAnd Password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
 const defaultFormFields = {
     displayName: '',
     email: '',
     password: '',
     confirmPassword: ''
-
 }
 
 const SignUpForm = () => {
@@ -15,6 +15,30 @@ const SignUpForm = () => {
 
     console.log(formFields)
 
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields)
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        if (password !== confirmPassword) {
+            alert('passwords do not match')
+            return
+        }
+
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(email, password)
+            await createUserDocumentFromAuth(user, { displayName })
+            resetFormFields()
+        } catch(error) {
+            if (error.code === 'auth/email-already-in-use') {
+                alert('Cannot create user, email already in use')
+            } else {
+                console.log(error)
+            }
+        }
+    }
+
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormFields({ ...formFields, [name] : value })
@@ -23,7 +47,7 @@ const SignUpForm = () => {
     return (
         <div>
             <h1>Sign up with your email and password</h1>
-            <form onSubmit={() => {}}>
+            <form onSubmit={handleSubmit}>
                 <label>Display Name</label>
                 <input type='text' required onChange={handleChange} name='displayName' value={displayName} /> 
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { 
+    getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword 
+} from 'firebase/auth'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
@@ -25,7 +27,8 @@ const firebaseConfig = {
 
   export const db = getFirestore()
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+      if (!userAuth) return
       const userDocRef = doc(db, 'users', userAuth.uid)
       const userSnapshot = await getDoc(userDocRef)
 
@@ -37,7 +40,8 @@ const firebaseConfig = {
               await setDoc(userDocRef, {
                   displayName,
                   email,
-                  createdAt
+                  createdAt,
+                  ...additionalInformation
               })
           } catch (error) {
               console.log('error creating the user', error.message)
@@ -45,4 +49,9 @@ const firebaseConfig = {
       }
 
       return userDocRef
+  }
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return
+    return await createUserWithEmailAndPassword(auth, email, password)
   }
