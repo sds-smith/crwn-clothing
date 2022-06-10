6e37a847f311e53ca394cbd06d3784f2f971e270
Shawn Smith <sds.smith24@gmail.com>
2022-06-10 11:56:55 -0400

    add cart context and cart dropdown toggle functionality

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,12 +6,15 @@ import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 
 import { UserContext } from '../../contexts/user.context'
+import { CartContext } from '../../contexts/cart.context'
+
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 import './navigation.styles.scss'
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext)
+    const { isCartOpen } = useContext(CartContext)
     
     return (
       <Fragment>
@@ -35,7 +38,7 @@ const Navigation = () => {
             }
             <CartIcon />
           </div>
-          <CartDropdown />
+          { isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </Fragment>
