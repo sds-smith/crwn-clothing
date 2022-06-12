4e426d8d9d427ee335e1f86c3fdefc7b0b30e8c7
Shawn Smith <sds.smith24@gmail.com>
2022-06-12 07:26:46 -0400

    convert button to styled component, add BUTTON_TYPE_CLASSES object

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './sign-in-form.styles.scss'
 import FormInput from '../form-input/form-input.component'
-import Button from '../button/button.component'
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component'
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
 
 const defaultFormFields = {
@@ -58,7 +58,7 @@ const SignInForm = () => {
                 <FormInput label='Password' type='password' required onChange={handleChange} name='password' value={password} /> 
                 <div className='buttons-container' >
                     <Button type='submit' >Sign In</Button>
-                    <Button type='button' onClick={signInWithGoogle} buttonType='google' >Google Sign In</Button>
+                    <Button type='button' onClick={signInWithGoogle} buttonType={BUTTON_TYPE_CLASSES.google} >Google Sign In</Button>
                 </div>
 
             </form>
