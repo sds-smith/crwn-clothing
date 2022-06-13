69beceba3191acdeee0b49402b14a15f6cb9c066
Shawn Smith <sds.smith24@gmail.com>
2022-06-13 13:48:56 -0400

    add createAction util

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,7 @@
 import { createContext, useReducer } from 'react'
 
+import {createAction} from '../utils/reducer/reducer.utils'
+
 const addCartItem = (cartItems, productToAdd) => {    
     const existingCartItem = cartItems.find(
         (cartItem) => cartItem.id === productToAdd.id
@@ -86,7 +88,13 @@ export const CartProvider = ({ children }) => {
         const newCartTotal = newCartItems.reduce(
             (total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
 
-        dispatch({ type: CART_ACTION_TYPES.SET_CART_ITEMS, payload: { cartItems: newCartItems, cartTotal: newCartTotal, cartItemsCount: newCartCount } })
+        dispatch(
+            createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+                cartItems: newCartItems, 
+                cartTotal: newCartTotal, 
+                cartItemsCount: newCartCount 
+            })
+        )
     }
 
     const addItemToCart = (productToAdd) => {
@@ -105,7 +113,7 @@ export const CartProvider = ({ children }) => {
     }
 
     const setIsCartOpen = (bool) => {
-        dispatch({ type: CART_ACTION_TYPES.SET_IS_CART_OPEN, payload: bool })
+        dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool))
     }
 
     const value = {
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
+import { createAction } from "../utils/reducer/reducer.utils";
 
 export const UserContext = createContext({
     currentUser : null,
@@ -38,7 +39,7 @@ export const UserProvider = ({ children }) => {
     console.log(currentUser)
 
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
     }
 
     const value = { currentUser, setCurrentUser }
