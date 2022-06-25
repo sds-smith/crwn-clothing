6e6e13124e4830d9130eb8ee3e69289d8fd48be9
Shawn Smith <sds.smith24@gmail.com>
2022-06-25 11:47:09 -0400

    install redux libraries, create store and new user reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './categories.styles.scss'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from 'react-router-dom'
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from './utils/firebase/firebase.utils'
+import { setCurrentUser } from "./store/user/user.action";
 import Navigation from './routes/navigation/navigation.component'
 import Home from './routes/home/home.component'
 import Authentication from './routes/authentication/authentication.component'
 import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
+import './categories.styles.scss'
+
 
 const App =  () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+        if (user) {
+            createUserDocumentFromAuth(user)
+        }
+        dispatch(setCurrentUser(user))
+    })
+    return unsubscribe
+  }, [])
   
   return (
     <Routes>
