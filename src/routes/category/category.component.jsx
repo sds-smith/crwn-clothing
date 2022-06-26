28a98e72f269c3c61b294944d0987e187d51ea39
Shawn Smith <sds.smith24@gmail.com>
2022-06-26 12:39:10 -0400

    install and implement reselect library

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -8,6 +8,7 @@ import './category.styles.scss'
 
 const Category = () => {
     const { category } = useParams()
+    
     const categoriesMap = useSelector(selectCategoriesMap)
 
     const [products, setProducts] = useState(categoriesMap[category])
--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,17 @@
-export const selectCategoriesMap = (state) => state.categories.categories    
-    .reduce((accumulator, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => categories.reduce((accumulator, category) => {
         const { title, items } = category
         accumulator[title.toLowerCase()] = items
         return accumulator
-    }, {});
+    }, {})
+)
