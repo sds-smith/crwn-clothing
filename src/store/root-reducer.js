6bca021b32939b4c93dc66a6db782a5e43043a15
Shawn Smith <sds.smith24@gmail.com>
2022-06-26 16:05:23 -0400

    install and implement redux-persist

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+
 import { userReducer } from "./user/user.reducer";
 import { categoriesReducer } from "./categories/category.reducer";
 import { cartReducer } from "./cart/cart.reducer";
