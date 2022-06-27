3e8bd696f63ba03c269c40714579f49ac47410d1
Shawn Smith <sds.smith24@gmail.com>
2022-06-27 13:20:29 -0400

    install redux saga and implement in categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import CategoriesPreview from '../categories-preview/categories-preview.component'
 import Category from '../category/category.component'
-import { fetchCategoriesAsync } from '../../store/categories/category.action'
+import { fetchCategoriesStart } from '../../store/categories/category.action'
 import './shop.styles.scss'
 
 const Shop = () => {
@@ -11,7 +11,7 @@ const Shop = () => {
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch(fetchCategoriesAsync())
+        dispatch(fetchCategoriesStart())
     }, [dispatch])
 
     return (
