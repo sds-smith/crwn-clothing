1630b999abf80ac6fa992604308bc97588d46306
Shawn Smith <sds.smith24@gmail.com>
2022-09-03 14:33:59 -0400

    convert utility files to ts, index.js to tsx

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,6 +1,14 @@
 declare module "*.svg" {
     import React = require('react')
     export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const content: any;
-    export default content;
+    export const content: any;
   }
+
+  declare namespace NodeJS {
+    interface ProcessEnv {
+      REACT_APP_STRIPE_PUBLISHABLE_KEY: string
+      STRIPE_SECRET_KEY: string
+    }
+    export const content: any;
+  }
+  
--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import App from './App';
 import './index.scss';
 import { store, persistor } from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store} >
