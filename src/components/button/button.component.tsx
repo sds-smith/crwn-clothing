ecceb598889d26bfafe6157e3be7c2829a872978
Shawn Smith <sds.smith24@gmail.com>
2022-09-05 08:18:56 -0400

    assign correct type to buttonprops.buttonType

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -13,11 +13,10 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseButton =>
         [BUTTON_TYPE_CLASSES.base]: BaseButton,
         [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
         [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-
     }[buttonType])
 
 type ButtonProps = {
-    buttonType?: string;
+    buttonType?: BUTTON_TYPE_CLASSES;
     isLoading?: boolean;
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
